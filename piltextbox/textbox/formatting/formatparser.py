ed80b9bf039436768a160952e3526f9434acb7ae
James P. Imes <jamesimes@gmail.com>
2020-10-05 18:47:43 -0700

    skeleton parsing

--- piltextbox/textbox/formatting/formatparser.py
+++ piltextbox/textbox/formatting/formatparser.py
@@ -1,13 +1,12 @@
 # Copyright (c) 2020, James P. Imes. All rights reserved.
 
-# TODO: Receive input text
-# TODO: Split text into words by spaces.
-# TODO: Find formatting codes (<b>, </b>, <i>, </i>).
-#       <b> --> bold=True
-#       </b> --> bold=False
-#       <i> --> ital=True
-#       </i> --> ital=False
-# TODO: encode each word into an FWord object.
+"""
+A naive parser, taking raw text with optional format codes '<b>',
+'</b>', '<i>', and '</i>' and generating a list of FWord objects.
+
+NOTE: Format codes can only exist at word boundaries (and must be
+outside punctuation).
+"""
 
 class FWord:
     """
@@ -18,3 +17,41 @@ class FWord:
         self.txt = txt
         self.bold = bold
         self.ital = ital
+
+
+def format_parse(text) -> list:
+    """
+    Split text into a list of FWord objects with encoded formatting.
+    """
+
+    format_codes = ('<b>', '</b>', '<i>', '</i>')
+
+    bold = False
+    ital = False
+    raw_words = text.split(' ')
+    fwords = []
+    for word in raw_words:
+        txt = word
+
+        while True:
+            if not txt.startswith(format_codes):
+                break
+            # TODO: Set `bold` and `ital` appropriately, and cull code from `txt`
+        
+        while True:
+            if not txt.endswith(format_codes):
+                break
+            # TODO: Set `bold` and `ital` appropriately, and cull code from `txt`
+
+        if len(txt) > 0:
+            fwords.append(FWord(txt, bold=bold, ital=ital))
+
+    return fwords
+
+
+wrds = format_parse('Testing one, two, three, four.')
+for wrd in wrds:
+    print(wrd)
+    print(wrd.txt)
+    print(wrd.bold)
+    print(wrd.ital)
