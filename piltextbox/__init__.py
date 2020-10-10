6c1a21d9f5361ae129aa8e9fddd4bffd741a563b
James P. Imes <jamesimes@gmail.com>
2020-10-09 22:19:09 -0700

    added website to module-level init

--- piltextbox/__init__.py
+++ piltextbox/__init__.py
@@ -13,5 +13,6 @@ __version_date__ = _constants.__version_date__
 __author__ = _constants.__author__
 __email__ = _constants.__email__
 __license__ = _constants.__license__
+__website__ = _constants.__website__
 
 from piltextbox.textbox import TextBox
