bd9912994ea32038a6353d652729ad426f4c9665
James P. Imes <jamesimes@gmail.com>
2020-10-09 22:18:22 -0700

    updated version info

--- piltextbox/_constants.py
+++ piltextbox/_constants.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2020, James P. Imes. All rights reserved.
 
-__version__ = '0.2.0.alpha'
-__version_date__ = '10/7/2020'
+__version__ = '0.2.0'
+__version_date__ = '10/9/2020'
 __author__ = 'James P. Imes'
 __email__ = 'jamesimes@gmail.com'
 __website__ = 'https://github.com/JamesPImes/piltextbox'
+
 __license__ = (
     'piltextbox is Copyright (c) 2020, James P. Imes, and is licensed'
     'under the MIT License -- see LICENSE.txt for full text, also'
