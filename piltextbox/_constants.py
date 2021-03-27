d796cf0431d954213a5335f985b9f3d5e26d8893
James P. Imes <jamesimes@gmail.com>
2021-03-26 23:14:41 -0700

    increment version

--- piltextbox/_constants.py
+++ piltextbox/_constants.py
@@ -1,7 +1,7 @@
-# Copyright (c) 2020, James P. Imes. All rights reserved.
+# Copyright (c) 2020-2021, James P. Imes. All rights reserved.
 
-__version__ = '0.2.0'
-__version_date__ = '10/10/2020'
+__version__ = '0.2.1'
+__version_date__ = '3/26/2021'
 __author__ = 'James P. Imes'
 __email__ = 'jamesimes@gmail.com'
 __website__ = 'https://github.com/JamesPImes/piltextbox'
